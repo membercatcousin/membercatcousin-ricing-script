28658a0b479bb9db02968bf357fc82133261f773
membercatcousin <membercatcousin@gmail.com>
2026-02-10 21:39:53 +0100

    Add yay installer to script.sh
    Added installation steps for yay package manager.

--- script.sh
+++ script.sh
@@ -1,3 +1,13 @@
 # -- membercatcousin's ricing script --
 
 echo "membercatcousin's ricing script Installer!"
+
+# -- yay installer --
+
+sudo pacman -Syu
+sudo pacman -S --needed base-devel git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+echo "verfiying yay..."
+yay --version
