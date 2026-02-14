fa12c9db3f53a8cc4a55e8dad405b432a1144005
membercatcousin <membercatcousin@gmail.com>
2026-02-14 14:40:15 +0100

    Add desktop environment selection prompt

--- de_installer.py
+++ de_installer.py
@@ -1,2 +1,21 @@
-print("too lazy to add will do it later")
-print("Im lazy too")
+print("""
+Desktop eviroment installer Select your desired Desktop enviroment:
+
+1) KDE Plasma
+2) Gnome
+3) xfce
+4) i3
+
+""")
+
+de_selected = input("""
+Type the selected desktop enviroment (eg: '1', '1-4', '1 3 2')
+
+==) 
+""")
+
+if de_selected == 1:
+  print("Good choice. but i'm too lazy to make it functional, i will do it later.")
+else:
+  print("too lazy to add will do it later")
+  print("Im lazy too (bkhr)")
