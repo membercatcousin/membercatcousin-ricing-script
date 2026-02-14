be92f703bf824ccc366ce698ee0c0cd33dd0137f
Robin <huzaiaprogmr@gmail.com>
2026-02-14 18:53:08 +0530

    ts gona be peak twin
    Updated comments for clarity and added humor.

--- script.sh
+++ script.sh
@@ -6,23 +6,24 @@ echo "membercatcousin's ricing script Installer!"
 
 # -- yay installer --
 
-# Check if yay is ALREADY installed so we don't waste time
+# Check if yay is ALREADY installed so we don't waste time and not get enuf food in the asian buffet
 if command -v yay &> /dev/null; then
     echo "✅ yay is already installed! Skipping..."
 else
     echo "⚠️ yay not found."
     read -p "Install yay?(y/n):" -n 1 -r
     if [[ $REPLY =~ [Yy]$ ]] then    
-    sudo pacman -Syu
-    sudo pacman -S --needed base-devel git
-    # Clone into /tmp so we don't clutter the user's home folder
-    git clone https://aur.archlinux.org/yay.git /tmp/yay
-    cd /tmp/yay
-    makepkg -si --noconfirm
+        sudo pacman -Syu
+        sudo pacman -S --needed base-devel git
+        # Clone into /tmp so we don't clutter the user's home folder and they call 911
+        git clone https://aur.archlinux.org/yay.git /tmp/yay
+        cd /tmp/yay
+        makepkg -si --noconfirm
     
-    # Clean up the installation files
-    cd ~
-    rm -rf /tmp/yay
+        # Clean up the installation files bcoz we gotta leave no evidence :skull:
+        cd ~
+        rm -rf /tmp/yay
+        fi
 fi
 
 
@@ -51,3 +52,5 @@ case $choice in
   6) sudo pacman -S microsoft-edge --noconfirm;;
   *) echo "Invalid choice:" ;;
 esac
+
+#DE installation (someone do it i cant)
