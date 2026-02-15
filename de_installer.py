182c67bce29409d9b3635fb9100b0ad6f9b73bcc
Robin <huzaiaprogmr@gmail.com>
2026-02-15 12:32:11 +0530

    Implement user input for Desktop Environment installation
    Added user prompt for installing a Desktop Environment with options.

--- de_installer.py
+++ de_installer.py
@@ -1,21 +1,29 @@
-print("""
-Desktop eviroment installer Select your desired Desktop enviroment:
-
-1) KDE Plasma
-2) Gnome
-3) xfce
-4) i3
-
-""")
-
-de_selected = input("""
-Type the selected desktop enviroment (eg: '1', '1-4', '1 3 2')
-
-==) 
-""")
-
-if de_selected == 1:
-  print("Good choice. but i'm too lazy to make it functional, i will do it later.")
-else:
-  print("too lazy to add will do it later")
-  print("Im lazy too (bkhr)")
+import subprocess
+#imma try loop for it rather than learining python UwU u can judge my part -Robin(lemon)
+con = input("Want to install a Desktop Environment?(y/n): )
+            if con == "Y" or "y":
+            #wrong indent the faq
+            #aaaaaaahhhhhhhhhh- lemon again
+                while True:
+                  print("""Choose a Desktop Environment
+                  1.KDE Plasma
+                  2.Gnome
+                  3.XFCE
+                  4.LXDE?
+                  5.i3
+                  6.Cancel""")
+                  #bro y does my keyboard refuse to type CANCLE ... see?-Lemon
+                  de = input("Choose an option (1/2/3...): ")
+                  if de = "1":
+                    #someone add these (my hands hurt aaaaaah)
+                    print("Installing KDE Plasma...")
+                    #subprocess stuff
+                  if de = "6":
+                    print("Done.Cancelling")
+                  else:
+                    print("Not a valid choice")
+                    continue
+                             # @github y are you giving me wrong indents crying emoji
+            else:
+                # i had to do it-lemon
+                print("Cancelling Installation(Desktop Environment)")
