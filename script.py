7e6c297e975ee4571ecc19463cbff24e09a7c353
Robin <huzaiaprogmr@gmail.com>
2026-02-22 20:27:50 +0530

    Add option to install additional packages
    Added prompt for installing additional packages and integrated apps_install function.

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 from desktop import de_install
+from addons import apps_install
 from browser import install
-bro = input("Do you want to install a browser(y/n): ")
+bro = input("Do you want to install a browser(y/n)?: ")
 if bro == "y" or "Y":
     install()
 else:
@@ -9,5 +10,10 @@ else:
 de = input("Do you want to install a Desktop environment(y/n)?: ")
 if de == "y" or "Y":
     de_install
+else:
+    print("Skipping")
+app = input("Want to install additional packages?(y/n): ")
+if app == "y" or "Y":
+    apps_install()
 else:
     print("Skipping")
